f28b13f37811c520f0caa38997cf754cee927202
unknown <20210112@I-BCCARD.BIZ>
2021-07-27 16:44:45 +0900

    prject creation

---
+++ src/main/java/com/pcr/demo/PcrApplication.java
@@ -0,0 +1,13 @@
+package com.pcr.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcrApplication.class, args);
+	}
+
+}
