72ffee91d2df21e29ae85a420238e790231e3741
20210112 <20210112@BCI20210112DS01.I-BCCARD.BIZ>
2021-07-29 11:16:58 +0900

    spring security authentication(AuthenticationManagerBuilder), authorization(HttpSecurity) completed

--- src/main/java/com/pcr/demo/security/SecurityConfig.java
+++ src/main/java/com/pcr/demo/security/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.pcr.security;
+package com.pcr.demo.security;
 
 import javax.sql.DataSource;
 
@@ -11,19 +11,24 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 @Autowired
 DataSource dataSource;
- @Override
+
+
+@Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         // it will check or tell us , what are the users, their passwords, and roles
 	    //first login : id| user , pw| generated security password(in cosole)
 	    //set id & pw in application.properties
 	    //AuthenticationManagerBuilder not resolved error -> security dependency3,4 added -> fixed
 	    //java.lang.NoClassDefFoundError: org/springframework/security/converter/RsaKeyConverters -> security dependency3 version deleted -> fixed
-        auth.inMemoryAuthentication()
+        //authentication
+	 	auth.inMemoryAuthentication()
                 .withUser("manish")
                 .password("123")
                 .roles("USER")
@@ -32,10 +37,10 @@ DataSource dataSource;
                 .password("1234")
                 .roles("ADMIN");
 
-        auth.jdbcAuthentication()
-                .dataSource(dataSource);
+        //auth.jdbcAuthentication()
+        //        .dataSource(dataSource);
     }
- 
+ //password hashing
  @Bean
  	public PasswordEncoder getPasswordEncoder(){
      return NoOpPasswordEncoder.getInstance();
@@ -44,11 +49,16 @@ DataSource dataSource;
  protected void configure(HttpSecurity http) throws Exception {
 	//it will configure the mappings, what kind of mapping are allowed for what users
 	//upper lines have priority 
-	 http.authorizeRequests()
-	 			.antMatchers("/admin").hasRole("ADMIN")
-	 			.antMatchers("/user").hasAnyRole("USER")
-	 			.antMatchers("/front").hasAnyRole("ADMIN","USER")
+	//authorization 
+	//when access denied, browser show 403 
+	 http.csrf().disable()    //disable it if the application users did not use it from browsers.
+	 			.authorizeRequests()
+	 			.antMatchers("/**").hasRole("ADMIN")  //people who has user role can access all pages
+				//.antMatchers("/**").hasAnyRole()  //all the people can access all pages
+				//.antMatchers("/admin").hasRole("ADMIN")
+	 			.antMatchers("/user").hasRole("USER")
+	 			//.antMatchers("/front").hasAnyRole("ADMIN","USER")
 	 			//.antMatchers("/**").permitAll()	
-	 			.and().formLogin();
+	 			  .and().formLogin();
  }
  }
