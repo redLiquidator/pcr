e425c481b836b3f4ce4833889c81f8e793bb3176
20210112 <20210112@BCI20210112DS01.I-BCCARD.BIZ>
2021-08-10 13:56:33 +0900

    redis 1.message broker 2.as database 3.cashing

--- src/main/java/com/pcr/demo/PcrApplication.java
+++ src/main/java/com/pcr/demo/PcrApplication.java
@@ -2,8 +2,10 @@ package com.pcr.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching  //turn on redis cashing
 public class PcrApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/pcr/demo/controller/FrontPageController.java
+++ src/main/java/com/pcr/demo/controller/FrontPageController.java
@@ -12,9 +12,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.pcr.demo.redis.DnaSequence;
-import com.pcr.demo.redis.DnaSequenceRepository;
-
 @Controller
 public class FrontPageController {
 
@@ -49,56 +46,4 @@ public class FrontPageController {
 		//DnaSequence is acquired through RestService (from the Github Prject name 'consumeRestService')
 		//This DnaSequence, will be analyzed by pcr system.
 		
-		//define a connectionFactory using Jedis client
-		@Bean
-		JedisConnectionFactory jedisConnectionFactory() {
-		    return new JedisConnectionFactory();
-		}
-		
-		//define a RedisTemplate using the jedisConnectionFactory. 
-		//This will be used for quering data with a custom repository.
-		//source https://www.baeldung.com/spring-data-redis-tutorial#maven-dependencies
-		@Bean
-		public RedisTemplate<String, Object> redisTemplate() {
-		    RedisTemplate<String, Object> template = new RedisTemplate<>();
-		    template.setConnectionFactory(jedisConnectionFactory());
-		    return template;
-		}
-		
-		@Autowired
-		DnaSequenceRepository dnaSequenceRepository;
-		
-		public DnaSequence DnaRepositorySave() {
-			DnaSequence dnaSequence = new DnaSequence(1,"CGAATAGCTAGGGATTTCTATA",22,new Date());
-			System.out.println("dnaSequence"+dnaSequence);
-			return dnaSequenceRepository.save(dnaSequence);
-		}
-		
-		public DnaSequence DnaRepositoryRetrieveOneData() {
-			DnaSequence retrievedDnaSequence = 
-					dnaSequenceRepository.findById(1).get();
-			return retrievedDnaSequence;
-		}
-		
-		public List<DnaSequence> DnaRepositoryRetrieveList() {
-			DnaSequence dnaSequence1 = new DnaSequence(2,"AATAGCTAGGGATTTCTATA",20,new Date());
-			DnaSequence dnaSequence2 = new DnaSequence(3,"CGAATAGCTAGTCTATA",18,new Date());
-				dnaSequenceRepository.save(dnaSequence1);
-				dnaSequenceRepository.save(dnaSequence2);
-				List<DnaSequence> seq = new ArrayList<>();
-		 dnaSequenceRepository.findAll().forEach(seq::add);
-		 return seq;
-					
-		}
-		
-		public DnaSequence DnaRepositoryUpdate() {
-			DnaSequence dnaSequence = new DnaSequence(1,"CGAATAGCTAGGGA",14,new Date());
-			dnaSequence.set("Richard Watson");
-			return dnaSequenceRepository.save(dnaSequence);
-		}
-		
-		public DnaSequence DnaRepositoryDelete() {
-			return dnaSequenceRepository.deleteById(1);
-			
-		}			
 }
