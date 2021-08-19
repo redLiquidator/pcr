c93a2659e1e6a1d7b1d71a36d77d40b1d1c8f630
unknown <20210112@I-BCCARD.BIZ>
2021-08-19 18:07:17 +0900

    prep1 completed

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -19,4 +19,17 @@ INSERT INTO PRIMER (id, name, sequence, creation_date, created_by) VALUES (6,'TR
 INSERT INTO PRIMER (id, name, sequence, creation_date, created_by) VALUES (7,'TRAV15D-1 F1','5-AAGTAAGAGGCTGTGGTAAGCTCT-3', now(), 'americano2021');
 INSERT INTO PRIMER (id, name, sequence, creation_date, created_by) VALUES (8,'TRAV15D-1 R1','5-TCTAAGTGCCTGCCACGTGT-3', now(), 'white' );
 INSERT INTO PRIMER (id, name, sequence, creation_date, created_by) VALUES (9,'TRAV15D-2 F1','5-ATGCACACTGCCTGTCTCTC-3', now(), 'americano2021' );
+INSERT INTO PRIMER (id, name, sequence, creation_date, created_by) VALUES (10,'SAMPLE1','5-ATG-3', now(), 'bejamin' );
+INSERT INTO PRIMER (id, name, sequence, creation_date, created_by) VALUES (11,'SAMPLE2','5-CTT-3', now(), 'bejamin' );
+INSERT INTO PRIMER (id, name, sequence, creation_date, created_by) VALUES (12,'SAMPLE3','5-GGC-3', now(), 'bejamin' );
+INSERT INTO PRIMER (id, name, sequence, creation_date, created_by) VALUES (13,'SAMPLE4','5-ATT-3', now(), 'bejamin' );
+INSERT INTO PRIMER (id, name, sequence, creation_date, created_by) VALUES (14,'SAMPLE5','5-GCC-3', now(), 'bejamin' );
+INSERT INTO PRIMER (id, name, sequence, creation_date, created_by) VALUES (15,'SAMPLE6','5-ACC-3', now(), 'bejamin' );
+INSERT INTO PRIMER (id, name, sequence, creation_date, created_by) VALUES (16,'SAMPLE7','5-CCG-3', now(), 'bejamin' );
+INSERT INTO PRIMER (id, name, sequence, creation_date, created_by) VALUES (17,'SAMPLE8','5-CGC-3', now(), 'bejamin' );
+INSERT INTO PRIMER (id, name, sequence, creation_date, created_by) VALUES (18,'SAMPLE9','5-GCT-3', now(), 'bejamin' );
+INSERT INTO PRIMER (id, name, sequence, creation_date, created_by) VALUES (19,'SAMPLE10','5-GGA-3', now(), 'bejamin' );
+INSERT INTO PRIMER (id, name, sequence, creation_date, created_by) VALUES (20,'SAMPLE11','5-ACA-3', now(), 'bejamin' );
+INSERT INTO PRIMER (id, name, sequence, creation_date, created_by) VALUES (21,'SAMPLE12','5-CCT-3', now(), 'bejamin' );
+INSERT INTO PRIMER (id, name, sequence, creation_date, created_by) VALUES (22,'SAMPLE13','5-CCC-3', now(), 'bejamin' );
 
--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,12 +1,27 @@
-//selectors
 
+
+
+//on clicking, get the position&base
+$('#prep1-result-one').click( function () {
+  getSelectionPosition (); 
+});
+
+
+function getSelectionPosition () {
+  var selection = window.getSelection();
+  console.log(selection.focusNode.data[selection.focusOffset]);
+  console.log(selection.focusOffset);
+}
+
+
+//selector
 let prep1 = document.querySelector("#prep1");
 
-//call PreparationController.getDnaSequence()
+//1.create a sequence of dna. Enter the length of dna you want to get: 		
 prep1.addEventListener("click", ()=>{
 	
 	console.log("prep1"+prep1);
-
+	//call PreparationController.getDnaSequence()
 	$.get("/prep1", 
 			{ dnaLength: $('#dLength').val()}, 
 			// 서버가 필요한 정보를 같이 보냄. 
@@ -29,8 +44,31 @@ prep1.addEventListener("click", ()=>{
 						complementarySeq += 'A';
 					};
 				}
-				$("#prep1-result-another").html('5\'-'+complementarySeq+'-3\''); 
-				
-				
+				$("#prep1-result-another").html('5\'-'+complementarySeq+'-3\''); 		
 			} ); 
+		
+		 //notice1, prep2-container
+		 $('#notice1').show();
+		 
+		 $('#prep2-subcontainer1').show();
+		
 		});
+
+
+	
+
+	 
+	/* base.addEventListener("mouseover", (e)=> {
+		console.log(e.target);
+	} ) */
+	
+//2.click the starting point of extension on the two complementary strands above. We will find suitable primers.		
+//3.now the system brings polymerase enzyme & buffer
+prep2.addEventListener("click", ()=>{
+
+	 console.log("prep2 starts");
+	 $('#prep2-subcontainer2').show();
+	 $('#prep2-subcontainer3').show();
+	});	
+	
+
