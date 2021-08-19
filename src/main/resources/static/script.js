141cbaff84699f958fd542f717615a55a52a818f
20210112 <20210112@BCI20210112DS01.I-BCCARD.BIZ>
2021-08-19 10:25:59 +0900

    prep1 completed

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -4,6 +4,7 @@ let prep1 = document.querySelector("#prep1");
 
 //call PreparationController.getDnaSequence()
 prep1.addEventListener("click", ()=>{
+	
 	console.log("prep1"+prep1);
 
 	$.get("/prep1", 
@@ -12,7 +13,24 @@ prep1.addEventListener("click", ()=>{
 			function(data, status) { 
 				console.log(data);
 				console.log(status);
-				//$("#text").html(data + "<br>" + status); 
-			// 전송받은 데이터와 전송 성공 여부를 보여줌. 
+				let seq = data.sequence;
+				$("#prep1-result-one").html('3\'-'+seq+'-5\''); 
+				
+				//get a complementary strand of prep1-result-one
+				let complementarySeq = "";
+				for(i=0;i<seq.length;i++){
+					if(seq.charAt(i) == 'C'){
+						complementarySeq += 'G';
+					}else if(seq.charAt(i) == 'G'){
+						complementarySeq += 'C';
+					}else if(seq.charAt(i) == 'A'){
+						complementarySeq += 'T';
+					}else{
+						complementarySeq += 'A';
+					};
+				}
+				$("#prep1-result-another").html('5\'-'+complementarySeq+'-3\''); 
+				
+				
 			} ); 
 		});
